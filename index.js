e4364ceef0f93123dd9c7c99149389f46a0422d5
Prasan RKP <prasan543e@gmail.com>
2025-11-08 12:15:37 +0530

    ready to delploy

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import authRoutes from '../server/src/routes/authRoute.js'
+
+const app = express();
+
+const PORT = process.env.PORT || 3008;
+
+app.use(express.json({limit: "10mb"}));
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
+
+mongoose.connect("mongodb://127.0.0.1:27017/3D-portfolio")
+.then(()=> {
+    console.log(`successfully connected to ${mongoose.connection.name}✅`);
+
+    app.listen(PORT, ()=>{
+         console.log(`Server is runing`)
+    });
+})
+
+.catch((err)=>{
+    console.log("Failed to connect with DB:", err);
+    process.exit(1);
+})
+
+
+// Routes are from here....
+app.use('/auth', authRoutes);
